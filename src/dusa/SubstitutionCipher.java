15848d5aa3364ccd280ffae553824673fc9264e8
Tobias Dusa <tdusa@student.tgm.ac.ac>
2018-10-20 18:23:20 +0200

    Finished SubstitutionCipher-Class

---
+++ src/dusa/SubstitutionCipher.java
@@ -0,0 +1,17 @@
+package dusa;
+
+
+/**
+ * SubstitutionCipher is extends the MonoAlphabeticCipher
+ * @author Tobias
+ * @version 20.10.2018
+ */
+public class SubstitutionCipher extends MonoAlphabeticCipher{
+	public SubstitutionCipher(String secretAlphabet) {
+		
+	}
+	
+	public void setSecretAlphabet() {
+		
+	}
+}
