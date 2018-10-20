f5008a51af0e02f83b024f85281e85ad3e565d83
Tobias Dusa <tdusa@student.tgm.ac.ac>
2018-10-20 18:19:57 +0200

    Created the Class MonoAlphabetic

---
+++ src/dusa/MonoAlphabeticCipher.java
@@ -0,0 +1,25 @@
+package dusa;
+
+public class MonoAlphabeticCipher {
+	private String secretAlphabet;
+	
+	public MonoAlphabeticCipher() {
+		this.secretAlphabet = "";
+	}
+
+	public String getSecretAlphabet() {
+		return secretAlphabet;
+	}
+
+	public void setSecretAlphabet(String secretAlphabet) {
+		this.secretAlphabet = secretAlphabet;
+	}
+	
+	public String encrypt(String text) {
+		
+	}
+	
+	public String decrypt(String text) {
+		
+	}
+}
