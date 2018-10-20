2d359276fb149687845313fd7f3b9355807dbdf9
Tobias Dusa <tdusa@student.tgm.ac.ac>
2018-10-20 18:25:20 +0200

    Finished the Interface

---
+++ src/dusa/Cipher.java
@@ -0,0 +1,11 @@
+package dusa;
+
+/**
+ * Cipher is the Interface
+ * @author Tobias
+ * @version 20.10.2018
+ */
+public interface Cipher {
+	public String encrypt(String text);
+	public String decrypt(String text);
+}
